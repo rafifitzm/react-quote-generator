9768aa732e317259b97e86a6f79e95f2bbf31b09
Rafi Fitzmaurice <rafi.fitzm@gmail.com>
2025-12-30 12:12:23 +0000

    Add project files

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-quote-generator
---
+++ lib/index.jsx
@@ -0,0 +1,61 @@
+const Quote = ({ quote }) => {
+  if (quote === null) {
+    return (
+      <div className="card p-3 my-3 bg-primary-subtle">
+        <p className="text-primary">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="card p-3 my-3 bg-primary-subtle">
+      <p className="text-primary">
+        <span>"</span>
+        {quote.content}
+        <span>"</span>
+      </p>
+      <span>- {quote.author}</span>
+    </div>
+  );
+};
+
+const App = () => {
+  const message = "If you see this message in your browser, that means React is successfully mounted! 🙌";
+
+  const [quotes, setQuotes] = React.useState([]);
+  const [currentQuote, setCurrentQuote] = React.useState(null);
+  console.log("Quotes: ", quotes);
+
+  const getRandomQuote = (quotes) => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  }
+
+  const getNewQuote = (quotes) => {
+    const newQuote = getRandomQuote(quotes);
+    setCurrentQuote(newQuote);
+  }
+
+  React.useEffect(() => {
+    fetch('https://quotable.vercel.app/quotes')
+      .then(response => response.json())
+      .then((data) => {
+        setQuotes(data.results);
+        getNewQuote(data.results);
+      });
+  }, []);
+
+
+  return (
+    <div id="quoteGeneratorContainer" className="container">
+      {message}
+      <Quote quote={currentQuote} />
+      <button className="btn btn-primary">More inspiration</button>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
